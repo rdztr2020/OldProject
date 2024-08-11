7dda41b51adf0315a7e46a99e8547f98d895a4bd
windowslee <slee12463@gmail.com>
2024-08-11 12:30:16 -0400

    add list remove functioning

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { addFood } from "../controllers/foodController.js";
+import { addFood, listFood,removeFood } from "../controllers/foodController.js";
 import multer from "multer";
 
 const foodRouter = express.Router();
@@ -7,22 +7,16 @@ const foodRouter = express.Router();
 //image storage engine
 
 const storage = multer.diskStorage({
-    destination:"uploads",
-    filename:(req,file,cb)=>{
-        return cb(null,`${Date.now()}${file.originalname}`)
+  destination: "uploads",
+  filename: (req, file, cb) => {
+    return cb(null, `${Date.now()}${file.originalname}`);
+  },
+});
 
-    }
-})
+export const upload = multer({ storage: storage });
 
-export const upload = multer({storage:storage})
-
-foodRouter.post("/add",upload.single("image"),addFood)
-
-
-
-
-
-
-
+foodRouter.post("/add", upload.single("image"), addFood);
+foodRouter.get("/list",listFood);
+foodRouter.post("/remove",removeFood)
 
 export default foodRouter;
